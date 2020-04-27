dc21d7489d79f1d2acfadd4c80ee0129ff056dee
travism26 <travismartin266@gmail.com>
2020-04-26 21:19:06 -0300

    update reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-    replaceMe: () => "WIP"
+  posts: postsReducer,
 });
